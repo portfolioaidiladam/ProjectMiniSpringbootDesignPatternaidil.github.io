5f9b78af8883358dc2a7eef97540c832474025ce
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-10-04 17:22:00 +0700

    first commit

---
+++ src/main/java/progmmerzamannow/spring/creational/prototype/Position.java
@@ -0,0 +1,7 @@
+package progmmerzamannow.spring.creational.prototype;
+
+public enum Position {
+
+  STAFF, MANAGER, VICE_PRESIDENT, C_LEVEL
+
+}
---
+++ src/test/java/progmmerzamannow/spring/creational/SpringDesignPatternsCreationalApplicationTests.java
@@ -0,0 +1,13 @@
+package progmmerzamannow.spring.creational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternsCreationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
